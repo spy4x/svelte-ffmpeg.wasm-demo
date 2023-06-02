61d98e573731011626201fc27606a8ffdd10f61b
Eirene <sorokina333@gmail.com>
2023-06-02 22:23:34 +0800

    ui: ui improvements for scenarios

--- src/routes/scenarios/[id]/+page.svelte
+++ src/routes/scenarios/[id]/+page.svelte
@@ -5,7 +5,7 @@
 	import { onMount } from 'svelte';
 	import type { Scenario } from '@prisma/client';
 	import { AsyncOperationStatus, EntityOperationType } from '@shared';
-	import { toastStore } from '@skeletonlabs/skeleton';
+	import {Avatar, Step, Stepper, toastStore} from '@skeletonlabs/skeleton';
 	import { goto } from '$app/navigation';
 
 	let id: string;
@@ -33,123 +33,133 @@
 	});
 </script>
 
-<div class="container h-full mx-auto flex justify-center items-center">
+<div class="container h-full mx-auto">
 	{#if scenario}
+		<div class="flex items-center mt-2 mb-10">
+			<a class="hover:opacity-50" href="/scenarios">
+				<svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 mr-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+					<path stroke-linecap="round" stroke-linejoin="round" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+				</svg>
+			</a>
+			<h1 class="h2">Edit scenario</h1>
+		</div>
 		<form
 			data-e2e="new-scenario-form"
 			on:submit|preventDefault={() => void scenarios.update(scenario)}
+			class="grid lg:grid-cols-3 gap-12"
 		>
-			<div class="card">
-				<header class="card-header">Edit scenario</header>
-				<section class="p-4">
-					<div class="flex flex-col gap-5">
-						<label>
-							<span>Title</span>
-							<input
+			<div>
+				<div class="card p-8 flex flex-col gap-5">
+					<label>
+						<span>Title</span>
+						<input
 								bind:value={scenario.title}
 								class="input"
 								type="text"
 								placeholder="Enter title"
-							/>
-						</label>
-						<label>
-							<span>Description</span>
-							<textarea
+						/>
+					</label>
+					<label>
+						<span>Description</span>
+						<textarea
 								bind:value={scenario.description}
 								class="textarea"
 								rows="5"
 								placeholder="Enter description"
-							/>
-						</label>
+						/>
+					</label>
 
-						<hr class="opacity-50" />
+					<hr class="opacity-50" />
 
-						<div class="flex flex-col gap-5">
-							<p>Actors</p>
-							{#each scenario.actors as actor, i}
-								<label>
-									<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
-										<div class="input-group-shim">#{i + 1}</div>
-										<input bind:value={actor} type="text" placeholder="Enter actors" />
-										<button
+					<div class="flex flex-col gap-5">
+						<p>Actors</p>
+						{#each scenario.actors as actor, i}
+							<label>
+								<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+									<div class="input-group-shim">#{i + 1}</div>
+									<input bind:value={actor} type="text" placeholder="Enter actors" />
+									<button
 											on:click={() =>
 												(scenario.actors = scenario.actors.filter((a, index) => index !== i))}
 											type="button"
 											class="variant-ghost-surface"
-										>
-											X
-										</button>
-									</div>
-								</label>
-							{/each}
-							<button
+									>
+										X
+									</button>
+								</div>
+							</label>
+						{/each}
+						<button
 								on:click={() => (scenario.actors = [...scenario.actors, ''])}
 								type="button"
 								class="btn variant-ghost-surface">Add actor</button
-							>
-						</div>
+						>
+					</div>
+				</div>
+			</div>
 
-						<hr class="opacity-50" />
-
-						<!-- scenes, similar to Actors, but each scene is a multiselect of actors + a text field "description" -->
-						<div class="flex flex-col gap-5">
-							<p>Scenes:</p>
+			<div class="col-span-2">
+				<div class="card">
+					<section class="p-8">
+						<Stepper stepTerm="Scene">
 							{#each scenario.scenes as scene, i}
-								<div>
-									<div>#{i + 1}</div>
-									<label>
-										<span>Actor:</span>
-										<select bind:value={scene.actor} class="select" placeholder="Select actor(s)">
-											<option value={undefined}>---No actor---</option>
-											{#each scenario.actors as actor, ai}
-												<option value={ai}>{actor}</option>
-											{/each}
-										</select>
-									</label>
-									<label>
-										<span>Description:</span>
-										<textarea
-											bind:value={scene.description}
-											class="textarea"
-											placeholder="Enter description"
-										/>
-									</label>
-									<button
-										on:click={() =>
+								<Step>
+									<svelte:fragment slot="header">Scene #{i+1}</svelte:fragment>
+									<div class="mt-12 mb-20 flex flex-col gap-4">
+										<label>
+											<span>Actor:</span>
+											<select bind:value={scene.actor} class="select" placeholder="Select actor(s)">
+												<option value={undefined}>---No actor---</option>
+												{#each scenario.actors as actor, ai}
+													<option value={ai}>{actor}</option>
+												{/each}
+											</select>
+										</label>
+										<label>
+											<span>Description:</span>
+											<textarea
+													bind:value={scene.description}
+													class="textarea"
+													placeholder="Enter description"
+											/>
+										</label>
+
+										<div class="flex items-center gap-4">
+											<button
+													on:click={() =>
 											(scenario.scenes = scenario.scenes.filter((a, index) => index !== i))}
-										type="button"
-										class="btn variant-ghost-surface"
-									>
-										Delete scene
-									</button>
-								</div>
-							{/each}
-							<button
-								on:click={() =>
+													type="button"
+													class="btn variant-filled-error"
+											>
+												Delete scene
+											</button>
+											<button
+													on:click={() =>
 									(scenario.scenes = [
 										...scenario.scenes,
 										{ actor: scenario.actors[0], description: '' }
 									])}
-								type="button"
-								class="btn variant-ghost-surface"
-							>
-								Add scene
-							</button>
-						</div>
-					</div>
-				</section>
-				<hr class="opacity-50" />
-				<footer class="card-footer p-4 flex justify-end gap-3">
-					<a href="/scenarios" class="btn variant-ringed-primary">Cancel</a>
-					<button class="btn variant-filled-primary">
-						{#if $scenarios.operations[scenario.id]?.type === EntityOperationType.UPDATE && $scenarios.operations[scenario.id]?.status === AsyncOperationStatus.IN_PROGRESS}
-							<Loading />
-							Saving...
-						{:else}
-							Save
-						{/if}
-					</button>
-				</footer>
+													type="button"
+													class="ml-auto btn variant-filled-secondary"
+											>
+												Add scene
+											</button>
+
+											<button class="btn variant-filled-primary">
+												{#if $scenarios.operations[scenario.id]?.type === EntityOperationType.UPDATE && $scenarios.operations[scenario.id]?.status === AsyncOperationStatus.IN_PROGRESS}
+													<Loading />
+													Saving...
+												{:else}
+													Save
+												{/if}
+											</button>
+										</div>
+									</div>
+								</Step>
+							{/each}
+						</Stepper>
+					</section>
+				</div>
 			</div>
 
 			<!-- <Debug data={scenario} />
